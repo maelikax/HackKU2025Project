4ff0a838b7acd09fb14b8ac35f20a410fffbd3fb
maelikax <mikaelanavarro21@gmail.com>
2025-04-05 01:45:55 -0500

    Revert "ignore"
    This reverts commit 5a388f1caf77bf7565674159f9dab472075e5b9d.

--- medTracker/src/components/InputField.jsx
+++
@@ -1,9 +0,0 @@
-const InputField = ({type, placeholder, icon}) => {
-  return (
-    <div className="input-wrapper">
-        <input type={type} placeholder={placeholder} className="input-field" required />
-    </div>
-  )
-}
-
-export default InputField
